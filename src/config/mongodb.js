dda936dbccedbdb895f46a8e1490a211f4a2e1e1
mujammil28 <64261184+mujammil28@users.noreply.github.com>
2024-12-12 00:42:26 +0530

    created indexes in mogodb

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -9,7 +9,8 @@ export const mongoConnect=()=>{
                 MongoClient.connect(url)
                 .then(clientInstance=>{
                     client= clientInstance
-                    console.log("MongoDb Connected!!!")
+                    console.log("MongoDb Connected!!!");
+                    createIndex(client.db())
                 })
                 .catch(err=>{
                     console.log(err);
@@ -20,3 +21,17 @@ export const getdb=()=>{
 
             return client.db();
 }
+
+
+const createIndex= async(db)=>
+    {
+       try{ 
+        
+        await db.collection("users").createIndex({name:"text"});
+        await db.collection("users").createIndex({type:"text"});
+            console.log("Indexs created")
+        }
+            catch(err){
+                console.log(err);
+            }
+}       
